271236c356be1c5cec7710c76d6d9d095a182422
Dylan <91755493+DylanselET32@users.noreply.github.com>
2022-11-01 17:49:49 -0300

    Creacion de tabla de Alumnos
    Todavia no anda bien, Hay un error que va a traer problemas, el año tiene una ñ y la ñ no anda bien

--- sistemaEscuela/index.php
+++ sistemaEscuela/index.php
@@ -1,13 +1,9 @@
 
 <?php include("db.php")?>
 <?php include("includes/header.php")?>
-<?php echo addAlumno('jorge','montero',12345678,3);?>
 
-<?php prueba()?>
-
-<h1>EMPIEZA</h1>
 <div class="container p-4 ">
-    
+    <!--
     <div class="row">
         <div class="col-md-6">
         <?php include("includes/formAlumno.php")?>
@@ -16,6 +12,9 @@
         <?php include("includes/formCalificacion.php")?>
         </div>
     </div>
+    -->
+    <?php include("includes/tablaAlumnos.php")?>
+
 </div>
 
 <?php include("includes/footer.php")?>
